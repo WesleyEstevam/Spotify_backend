b6fe7461653941ee1859980ef25036a3ad424f15
WesleyEstevam <wesley.estevam@utei.com.br>
2022-11-09 15:59:55 -0300

    Faltam 3 rotas a serem desenvolvidas

--- src/index.js
+++ src/index.js
@@ -2,50 +2,93 @@ const express = require('express');
 const server = express();
 const port = 3000;
 const usuarios = require('./model/usuarios');
+const playlists = require('./model/playlists');
 const bodyParser = require('body-parser');
 
 //CONFIGURAÇÕES DO SERVIDOR
 server.use(bodyParser.urlencoded({extended: false}));
-server.use(bodyParser.json());
+server.use(bodyParser.json()); //convertendo tudo que vem com JSON
+server.use(express.json());
 
 //ROTAS DISPONÍVEIS
-//========================== Listagem de usuários ===========================
-server.get('/login', (req, res) => {
+//========================== Listagem da playlists =========================== OK
+server.get('/playlists', (req, res) => {
     try {
-        let lista = usuarios.map((user) => {
-            return user;
-        })
-        res.json(lista);
+        res.json(playlists);
 
     }catch (error){
         res.json(error);
     } 
 });
 
-//========================== Cadastro de usuários ===========================
+//========================== Detalhe da playlists com id =========================== OK
+server.get('/playlists/:id', (req, res) => {
+    let id = req.params.id;
+    let playlist = {};
+    playlist = playlists[id];
+    res.json(playlist);
+})
 
-server.post('/cadastro', (req, res) => {
+//========================== Cadastrar playlist =========================== OK
+
+server.post('/playlists', (req, res) => {
+    let { id, titulo, nomeMusica, artista } = req.body;
+    let novaPlaylist = playlists.push({ id, titulo, nomeMusica, artista });
+    res.json(novaPlaylist);
+})
+
+//========================== Editar playlist ===========================
+
+server.put('/playlists', (req, res) => {
+    
+})
+
+//========================== Cadastro de usuários =========================== OK
+
+server.post('/usuarios', (req, res) => {
     try{
         let { email, nome, senha, id } = req.body;
-        let novoUsuario = usuarios.push( { email, nome, senha, id } ).toString();
-
-        res.json(novoUsuario);
+        usuarios.push( { email, nome, senha, id } );
+        res.json(usuarios);
     }catch(error) {
         console.log(error)
     }
 
 })
 
-//========================== Alteração de usuários ===========================
+//========================== Login =========================== INCOMPLETA
+server.get('/usuarios?email=', (req, res) => {
+    let email = req.query['email'];
+    if (email ) {
+        
+    } else {
+        
+    }
+    let usuarios = usuarios[email];
+    res.json(usuarios);
+});
 
-server.put('/editarLogin/:id', (req, res) => {
+//========================== Listagem de usuários =========================== 
+
+server.get('/usuarios', (req, res) => {
+    try {
+        res.json(usuarios);
+
+    }catch (error){
+        res.json(error);
+    } 
+});
+
+//========================== Editar usuário =========================== OK
+
+server.put('/usuarios/:id', (req, res) => {
     let id = req.params.id;
     let { email, nome, senha } = req.body
-    let novoUsuario = id;
+    let novoUsuario = usuarios.find( user => user.id == id );
 
     if( novoUsuario ) {
-        novoUsuario.nome = nome;
         novoUsuario.email = email;
+        novoUsuario.nome  = nome;
         novoUsuario.senha = senha;
         
         res.json(novoUsuario);
@@ -55,14 +98,11 @@ server.put('/editarLogin/:id', (req, res) => {
 })
 
 
+//========================== Buscar músicas ===========================
 
-
-
-
-
-
-
-
+server.post('/musicas', (req, res) => {
+    
+})
 
 //VERIFICAÇÃO DO SERVIDOR
 server.listen(port, () => {
---
+++ src/model/playlists.js
@@ -0,0 +1,29 @@
+
+let playlists = [
+  {
+    id: 1,
+    titulo: 'This is Bilie Elish',
+    nomeMusica: 'All good girls',
+    artista: 'Billie elish',
+  },
+  {
+    id: 2,
+    titulo: 'You make me hate this city',
+    nomeMusica: 'You make me hate this city',
+    artista: 'Billie elish and Dua Lipa',
+  },
+  {
+    id: 3,
+    titulo: 'Nowdays paranoia',
+    nomeMusica: 'Nowdays paranoia',
+    artista: 'Billie elish and Post malone',
+  },
+  {
+    id: 4,
+    titulo: 'Think out',
+    nomeMusica: 'Think out',
+    artista: 'Billie elish',
+  }
+]
+
+module.exports = playlists;
