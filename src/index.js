29cd6989aa0cc8fcad4ced1cb9b5a75dcb9de985
Soter Garcias <soter_garcia@hotmail.com>
2022-12-01 22:01:18 -0300

    musicas

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const server = express();
 const port = 3001;
 
-const { connect } = require('./conexaoDB/conect');
+const connect = require('./conexaoDB/conect');
 
 //CONFIGURAÇÕES DO SERVIDOR
 server.use(express.json());
@@ -12,7 +12,7 @@ server.use(cors());
 //========================== Listagem da playlists =========================== OK
 server.get('/playlists', async (req, res) => {
 
-    const conn  = await connect();
+    const conn  = await connect;
     const [row] = await conn.query('SELECT * FROM playlist;');
 
     res.json(row);
@@ -23,7 +23,7 @@ server.get('/playlists', async (req, res) => {
 server.get('/playlists/:id', async (req, res) => {
     let id = req.params.id;
 
-    const conn  = await connect();
+    const conn  = await connect;
     const [row] = await conn.query(`SELECT * FROM playlist WHERE id_playlist=${id};`);
 
     res.json(row);
@@ -35,7 +35,7 @@ server.get('/playlists/:id', async (req, res) => {
 server.post('/playlists', async (req, res) => {
     let { titulo, artista, fk_musicas } = req.body;
 
-    const conn = await connect();
+    const conn = await connect;
 
     const [row] = await conn.query(`
     INSERT INTO playlist ( titulo, artista, fk_musicas ) 
@@ -50,7 +50,7 @@ server.put('/playlists/:id_playlist', async (req, res) => {
     let { titulo, artista, fk_musicas } = req.body;
     let id_playlist = req.params.id_playlist;
 
-    const conn = await connect();
+    const conn = await connect;
     const [row] = await conn.query(`
     UPDATE playlist SET titulo='${titulo}', artista='${artista}', fk_musicas='${fk_musicas}' 
     WHERE id_playlist = ${id_playlist};`)
@@ -80,7 +80,7 @@ server.post('/usuarios', async (req, res) => {
 server.get('/usuario', async (req, res) => {
     let queryEmail = req.query['email'];
 
-    const conn  = await connect();
+    const conn  = await connect;
     const [row] = await conn.query(`SELECT * FROM usuario WHERE email='${queryEmail}';`);
 
     res.json(row);
@@ -90,7 +90,7 @@ server.get('/usuario', async (req, res) => {
 //========================== Listagem de usuários =========================== OK
 
 server.get('/usuarios', async (req, res) => {
-    let conn  = await connect();
+    let conn  = await connect;
     let [row] = await conn.query('SELECT * FROM usuario;');
 
     res.json(row);
@@ -104,7 +104,7 @@ server.put('/usuarios/:id_usuario', async (req, res) => {
     let { nome, email, senha } = req.body
 
     if( id_usuario ) {
-        const conn  = await connect();
+        const conn  = await connect;
         const [row] = await conn.query(`
         UPDATE usuario SET nome='${nome}', email='${email}', senha='${senha}' 
         WHERE id_usuario = ${id_usuario};`)
@@ -122,10 +122,11 @@ server.get('/musicas', async (req, res) => {
     
     let pesquisa = req.query['nome'];
     
-    const conn = await connect(); //CONEXÃO COM O MYSQL
-    const [row]  = await conn.query(`SELECT * FROM musicas WHERE nome='${pesquisa}';`);
+    const conn = await connect; //CONEXÃO COM O MYSQL
+    const [row]  = await conn.query(`SELECT * FROM musicas WHERE nome LIKE '%${pesquisa}%';`);
 
-    res.json(row.includes)
+
+    res.json(row)
 
 })
 
