a02d9c811697c7de2ec98f2338e508e777b62230
Soter Garcias <soter_garcia@hotmail.com>
2022-12-01 21:08:08 -0300

    mudulo

--- src/conexaoDB/conect.js
+++ src/conexaoDB/conect.js
@@ -3,11 +3,11 @@ async function connect(){
         return global.connection;
  
     const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection("mysql://root:so97719571@localhost:3306/Web");//Alterar URL
+    const connection = await mysql.createConnection("mysql://root:so97719571@localhost:3306/spotify");//Alterar URL
 
     console.log("Conectou no MySQL!");
     global.connection = connection;
     return connection;
 }
 
-module.exports = {connect};
+module.exports = connect();
